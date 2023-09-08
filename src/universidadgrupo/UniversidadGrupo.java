98befb3e97d9186e991eec47de6919f26dbdc272
jesicaJC <jesicacondo092@gmail.com>
2023-09-08 16:50:46 -0300

    Initial commit

---
+++ src/universidadgrupo/UniversidadGrupo.java
@@ -0,0 +1,15 @@
+
+package universidadgrupo;
+
+import java.sql.Connection;
+import universidadgrupo.accesoAdatos.Conexion;
+
+
+public class UniversidadGrupo {
+
+    
+    public static void main(String[] args) {
+      Connection con= Conexion.getConexion();
+      
+    }
+}
---
+++ src/universidadgrupo/accesoAdatos/Conexion.java
@@ -0,0 +1,38 @@
+
+package universidadgrupo.accesoAdatos;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
+public class Conexion {
+     private static final String URL="jdbc:mariadb://localhost/";
+     private static final String BD="universidadulp";
+     private static final String USUARIO="root";
+     private static final String CONTRASEÑA="";
+     private static Connection connection;
+     
+     private Conexion(){}
+     
+     public static Connection getConexion(){ //NO o va devolver un objeto conexion de la clase. Va a devolver un objeto tipo Connection
+         
+        if(connection == null){ //es la primeras vez que vamos a invocar este metodo ahi se carga los drivrr de conexion.
+            
+            try {
+            Class.forName("org.mariasdb.jdbc.Driver");
+            connection = DriverManager.getConnection(URL+BD,USUARIO,CONTRASEÑA);
+            
+            JOptionPane.showInternalMessageDialog(null, "Conectado");
+            
+            } catch (ClassNotFoundException ex) {
+               JOptionPane.showInternalMessageDialog(null, "Error al cargar Driver");
+            } catch (SQLException ex) {
+                JOptionPane.showInternalMessageDialog(null, "Error de conexion"); 
+            }
+        
+        }
+        return connection;
+     }
+}
